a7f81b49798889670cf3b5c19d462d4089ef2a76
laihue <laithue2005@gmail.com>
2024-03-14 23:01:53 +0700

    upload my source code

--- js/love.js
+++ js/love.js
@@ -11,10 +11,11 @@
         return p1.add(p2).add(p3);
     }  
 
-    function inheart(x, y, r) {
+    function inheart(x, y,r) {
         // x^2+(y-(x^2)^(1/3))^2 = 1
         // http://www.wolframalpha.com/input/?i=x%5E2%2B%28y-%28x%5E2%29%5E%281%2F3%29%29%5E2+%3D+1
         var z = ((x / r) * (x / r) + (y / r) * (y / r) - 1) * ((x / r) * (x / r) + (y / r) * (y / r) - 1) * ((x / r) * (x / r) + (y / r) * (y / r) - 1) - (x / r) * (x / r) * (y / r) * (y / r) * (y / r);
+        //var z=(x*x)/(r*r)+(y*y)/(r*r);
         return z < 0;
     }
 
